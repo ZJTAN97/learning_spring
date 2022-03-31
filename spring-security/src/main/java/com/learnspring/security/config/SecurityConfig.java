4539d2be61417015baedd77686ea07f5c63969e6
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2022-03-31 23:32:34 +0800

    learnt about the jackson project

--- spring-security/src/main/java/com/learnspring/security/config/SecurityConfig.java
+++ spring-security/src/main/java/com/learnspring/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.learnspring.security.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -22,6 +23,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.withUser(users.username("Mary").password("test123").roles("MANAGER"))
 			.withUser(users.username("Susan").password("test123").roles("ADMIN"));
 		
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+				.loginPage("/login")
+				.loginProcessingUrl("/authenticateTheUser")
+				.permitAll();
 	}
 	
 }
