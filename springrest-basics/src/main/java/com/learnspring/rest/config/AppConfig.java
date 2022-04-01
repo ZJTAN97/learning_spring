be7315d0fbdbe9c527ab49e42a19a5eab4210b1a
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2022-04-01 10:41:11 +0800

    learnt about the basics of spring rest

---
+++ springrest-basics/src/main/java/com/learnspring/rest/config/AppConfig.java
@@ -0,0 +1,12 @@
+package com.learnspring.rest.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.learnspring.rest")
+public class AppConfig {
+
+}
---
+++ springrest-basics/src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<hr>
+
+<a href="test/hello">Hello</a>
+
+<br><br>
+
+<a href="api/students">Students</a>
+
+</body>
+</html>
