5c7d617cd1975aac19fc08be77621ad36bb0a156
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2022-04-29 16:49:39 +0800

    re-learning testing

---
+++ springboot-testing-guide/src/main/java/com/springboot/testguide/SpringbootTestingGuideApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.testguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestingGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTestingGuideApplication.class, args);
+	}
+
+}
