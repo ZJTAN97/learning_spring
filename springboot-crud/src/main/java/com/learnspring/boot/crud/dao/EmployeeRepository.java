2e7446ade3f4daa625adbcc3ff969e7fce201cbb
ZJ <zhijietan97@gmail.com>
2022-09-24 12:08:41 +0800

    add: microservices spring cloud server

--- springboot-crud/src/main/java/com/learnspring/boot/crud/dao/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.learnspring.boot.crud.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.learnspring.boot.crud.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-	// 
-}
--- springmvc-basics/src/main/webapp/index.jsp
+++
@@ -1,1 +0,0 @@
-<% response.sendRedirect("/springmvc-basics/home"); %>
