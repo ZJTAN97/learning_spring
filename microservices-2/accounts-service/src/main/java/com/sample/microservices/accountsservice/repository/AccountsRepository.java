7d67e2eb293fdc72ac0694e1b1470f8a1b14a268
ZJ <zhijietan97@gmail.com>
2022-09-25 23:52:45 +0800

    add: new microservices project

---
+++ microservices-2/accounts-service/src/main/java/com/sample/microservices/accountsservice/repository/AccountsRepository.java
@@ -0,0 +1,12 @@
+package com.sample.microservices.accountsservice.repository;
+
+import com.sample.microservices.accountsservice.model.Accounts;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends CrudRepository<Accounts, Long> {
+
+    Accounts findByCustomerId(int customerId);
+
+}
---
+++ microservices-2/accounts-service/src/test/java/com/sample/microservices/accountsservice/AccountsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.microservices.accountsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
