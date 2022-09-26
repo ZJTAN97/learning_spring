94bf589901a0130b948defe33f26e146b451462d
ZJ <zhijietan97@gmail.com>
2022-09-26 23:21:03 +0800

    progress

---
+++ microservices-2/cards-service/src/main/java/com/sample/microservices/cardsservice/model/Cards.java
@@ -0,0 +1,44 @@
+package com.sample.microservices.cardsservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cards {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "card_id")
+    private int cardId;
+
+    @Column(name = "customer_id")
+    private int customerId;
+
+    @Column(name = "card_number")
+    private String cardNumber;
+
+    @Column(name = "card_type")
+    private String cardType;
+
+    @Column(name = "total_limit")
+    private int totalLimit;
+
+    @Column(name = "amount_used")
+    private int amountUsed;
+
+    @Column(name = "availabile_amount")
+    private int availableAmount;
+
+    @Column(name = "create_dt")
+    private Date createdDt;
+
+}
---
+++ microservices-2/loans-service/src/main/java/com/sample/microservices/loansservice/controller/LoansController.java
@@ -0,0 +1,24 @@
+package com.sample.microservices.loansservice.controller;
+
+import com.sample.microservices.loansservice.model.Customer;
+import com.sample.microservices.loansservice.model.Loans;
+import com.sample.microservices.loansservice.repository.LoansRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class LoansController {
+
+    @Autowired
+    private LoansRepository loansRepository;
+
+    @PostMapping("/myLoans")
+    public List<Loans> getLoansDetails(@RequestBody Customer customer) {
+        return loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+    }
+
+}
