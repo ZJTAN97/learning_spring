4e00e76dd681626ac1827d4c8318f1a17518b772
ZJ <zhijietan97@gmail.com>
2022-09-29 09:49:20 +0800

    chore: removed all broken projects

--- microservices/currency-exchange-service/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO currency_exchange
-(id, currency_from, currency_to, conversion_multiple, environment)
-values(10001, 'USD', 'INR', 65, '8000');
-INSERT INTO currency_exchange
-(id, currency_from, currency_to, conversion_multiple, environment)
-values(10004, 'AUD', 'INR', 25, '8000');
